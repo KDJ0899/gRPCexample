994f8284bba14f4a5a003d26bee474d982fef394
Kim DongJin <kxv1234@naver.com>
2020-11-30 20:06:14 +0900

    서버에 Calculate 기능  추가 - Plus, Minus, Multi, Divide

---
+++ ex_server/calculate/calculate.go
@@ -0,0 +1,78 @@
+package calculate
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	pb "github.com/KDJ0899/gRPCexample/ex_pb"
+)
+
+//Server hi server
+type Server struct{}
+
+type operator interface {
+	calculate([]int64) (int64, error)
+}
+
+type plus struct{}
+type minus struct{}
+type multi struct{}
+type divide struct{}
+
+func (*plus) calculate(nums []int64) (int64, error) {
+	var result int64 = 0
+	for _, value := range nums {
+		result += value
+	}
+	return result, nil
+}
+
+func (*minus) calculate(nums []int64) (int64, error) {
+	var result int64 = 0
+	for _, value := range nums {
+		result -= value
+	}
+	return result, nil
+}
+
+func (*multi) calculate(nums []int64) (int64, error) {
+	var result int64 = 0
+	for _, value := range nums {
+		result *= value
+	}
+	return result, nil
+}
+
+func (*divide) calculate(nums []int64) (int64, error) {
+	var result int64 = 0
+	for key, value := range nums {
+		if key > 0 && value == 0 {
+			return 0, fmt.Errorf("division by zero")
+		}
+		result += value
+	}
+	return result, nil
+}
+
+//Calculate calculate Numbers
+func (s *Server) Calculate(ctx context.Context, in *pb.CalculateRequest) (*pb.CalculateResponse, error) {
+	log.Printf("Numbers: %v  Operator %s", in.GetNumbers(), in.GetOperator().String())
+
+	numbers := in.GetNumbers()
+	operators := map[pb.CalculateRequest_Operator]operator{
+		pb.CalculateRequest_PLUS:  &plus{},
+		pb.CalculateRequest_MINUS: &minus{},
+		pb.CalculateRequest_MUL:   &multi{},
+		pb.CalculateRequest_DIV:   &divide{},
+	}
+	op := operators[in.GetOperator()]
+
+	result, err := op.calculate(numbers)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.CalculateResponse{Result: result}, nil
+}
--- ex_server/main.go
+++ ex_server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"gRPCexample/ex_server/calculate"
 	"gRPCexample/ex_server/hi"
 	"log"
 	"net"
@@ -11,19 +12,30 @@ import (
 
 const (
 	port = ":50051"
+
+	Hi = iota
+	Caculate
 )
 
 // server is used to implement helloworld.GreeterServer.
 
 func main() {
+
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
-
 	s := grpc.NewServer()
-	pb.RegisterHiServer(s, &hi.Server{})
+	connectServer(s, Caculate)
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
+	}
+}
+
+func connectServer(s *grpc.Server, srvType int) {
+	if srvType == Hi {
+		pb.RegisterHiServer(s, &hi.Server{})
+	} else if srvType == Caculate {
+		pb.RegisterCalculateServer(s, &calculate.Server{})
 	}
 }
